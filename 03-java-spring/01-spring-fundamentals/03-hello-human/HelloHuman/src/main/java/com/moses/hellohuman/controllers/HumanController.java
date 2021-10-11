7c5c5ffc4e6fb10961cb496a8192ac50e898a8af
Moses Negus <35280933+mosesnegus@users.noreply.github.com>
2021-10-11 03:16:24 -0700

    Add files via upload

---
+++ 03-java-spring/01-spring-fundamentals/03-hello-human/HelloHuman/src/main/java/com/moses/hellohuman/controllers/HumanController.java
@@ -0,0 +1,14 @@
+package com.moses.hellohuman.controllers;
+
+import org.springframework.sterotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotations.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HumanController {
+	@RequestMapping("/")
+	public String index(@RequestParam(value="name", requred=false, viewMode.add
+	Attribute("name", name);))
+
+}

434604bf2844d2f0698033cc411d82d0a0163bd9
Moses Negus <35280933+mosesnegus@users.noreply.github.com>
2021-10-11 08:38:29 -0700

    Add files via upload

---
+++ 03-java-spring/05-login-and-registration/01-controller-and-views/controllerviews/src/main/java/com/moses/displaydate/ControllerviewsApplication.java
@@ -0,0 +1,13 @@
+package com.moses.displaydate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllerviewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControllerviewsApplication.class, args);
+	}
+
+}
